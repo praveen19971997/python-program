55ce6240c8316ca654f7cc667a3a5e304c55cfdf
praveen19971997 <praveensahu20212021@gmail.com>
2023-04-20 12:06:46 +0530

    python file added

---
+++ datatype.py
@@ -0,0 +1,18 @@
+#list datatype
+fruit=["mango","banana","orange"]
+print(fruit[0])#print mango
+
+#tuple datatype
+#these re immutable
+product=("xbox",506)
+print(product[1])#print 506
+
+#set is unordered collection unique datatype
+student_id={1,2,3,4}
+print(student_id)
+print(type(student_id))
+
+#dictionary is ordered collection of item with key value pair
+capital={'nepal':'kathmandu','india':'delhi'}
+print(capital['nepal'])
+
